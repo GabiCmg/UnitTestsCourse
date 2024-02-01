fb9b2e6c2a5e7dc2ae56219a49c476c776326f79
GabiCmg <gabriela.camaroli@gmail.com>
2024-01-31 23:28:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using UnitTestsCourse;
+
+WorldsDumbestFunctionTests.WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString();
---
+++ WorldsDumbestFunctionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsCourse
+{
+    internal class WorldsDumbestFunctionTests
+    {
+        // Naming Convention - ClassName_MethodName_ExpetedResult
+        public static void WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString()
+        {
+            try
+            {
+                //Arange = go get your variable, whatever you need, your classes, go get functions
+                int num = 0;
+                WorldsDumbestFunction worldsDumbest = new WorldsDumbestFunction();
+
+                //Act = execute this function
+                string result = worldsDumbest.ReturnsPikachuIfZero(num);
+
+                //Assert = whatever ever is returned is it what you want
+                if(result == "PIKACHU!")
+                {
+                    Console.WriteLine("PASSED: WorldsDumbestFunction.ReturnsPikachuIfZero_ReturnString");
+                }
+                else 
+                {
+                    Console.WriteLine("FAILED: WorldsDumbestFunction.ReturnsPikachuIfZero_ReturnString");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
